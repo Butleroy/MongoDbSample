8c801f9667d63eb927f77ce3f37f913bdb89665d
PhilJay <phil.jahoda@gmail.com>
2016-11-24 16:11:16 +0100

    Change collection names

--- MongoDbBookstore/src/io/myalfred/mongodb/databases/Collections.java
+++ MongoDbBookstore/src/io/myalfred/mongodb/databases/Collections.java
@@ -2,7 +2,7 @@ package io.myalfred.mongodb.databases;
 
 public abstract class Collections {
 
-	public static final String TEST_DATA_BOOKSTORE = "testdata_bookstores";
-	public static final String TEST_DATA_BOOK = "testdata_books";
-	public static final String TEST_DATA_AUTHOR = "testdata_authors";
+	public static final String TEST_DATA_BOOKSTORE = "bookstores";
+	public static final String TEST_DATA_BOOK = "books";
+	public static final String TEST_DATA_AUTHOR = "authors";
 }
